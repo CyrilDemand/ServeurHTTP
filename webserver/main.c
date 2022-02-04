8fef8716ea378c224c91db49416596375fd4ce43
Noe Delcroix <noe.delcroix.etu@univ-lille.fr>
2022-02-04 14:14:38 +0100

    sigpipe

--- webserver/main.c
+++ webserver/main.c
@@ -7,10 +7,14 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <signal.h>
+
 
 
 int main(){
 	int socket_serveur=creer_serveur(8080);
+
+	initialiser_signaux();
 
 	int socket_client;
 	if (socket_serveur==-1){
@@ -27,9 +31,17 @@ int main(){
 
 		for (int i=0;i<10;i++){
 			const char *message_bienvenue = "Bonjour, bienvenue sur mon serveur\n";
+			
 			write(socket_client, message_bienvenue, strlen(message_bienvenue));
+			
 			sleep(1);
 		}
 		
+	}
+}
+
+void initialiser_signaux(void){
+	if (signal(SIGPIPE, SIG_IGN) == SIG_ERR){
+		perror("signal");
 	}
 }
--- webserver/main.h
+++ webserver/main.h
@@ -1,5 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void initialiser_signaux(void);
+
 #endif
 
